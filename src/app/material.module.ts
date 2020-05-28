8aae1fcda766bc1d3539bd98e4eaa7c74c8bee6a
Ben Halverson <benhalverson33@gmail.com>
2020-05-27 21:34:40 -0700

    setup angular router

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
-  imports: [],
-  exports: []
+  imports: [
+    MatButtonModule,
+    MatButtonModule
+  ],
+  exports: [MatButtonModule, MatIconModule]
 })
 export class MaterialModule { }

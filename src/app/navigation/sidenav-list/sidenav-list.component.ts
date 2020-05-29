aba2d17588bbc795ecf769aa7c19ef415388de25
Ben Halverson <benhalverson33@gmail.com>
2020-05-28 23:43:37 -0700

    refactor(navigation):
    created new components called sidebar and header

---
+++ src/app/navigation/sidenav-list/sidenav-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-sidenav-list',
+  templateUrl: './sidenav-list.component.html',
+  styleUrls: ['./sidenav-list.component.scss']
+})
+export class SidenavListComponent implements OnInit {
+  @Output() closeSidenav = new EventEmitter<void>();
+  constructor() {}
+  onClose(): void {
+    this.closeSidenav.emit();
+  }
+  ngOnInit(): void {}
+}

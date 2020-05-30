afb0b0b9cdd6b91a7e890219d3345415a63cce3d
Ben Halverson <benhalverson33@gmail.com>
2020-05-29 19:35:07 -0700

    dialog completed

--- src/app/training/current-training/current-training.component.ts
+++ src/app/training/current-training/current-training.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { StopTrainingComponent } from './stop-training.component';
 
@@ -8,9 +8,15 @@ import { StopTrainingComponent } from './stop-training.component';
   styleUrls: ['./current-training.component.scss']
 })
 export class CurrentTrainingComponent implements OnInit {
+  @Output() trainingExit = new EventEmitter<void>();
   progress = 0;
   timer: number;
+
   constructor(private dialog: MatDialog) {}
+
+  /**
+   * Method to stop the progress bar timer.
+   */
   onStop(): void {
     clearInterval(this.timer);
     const dialogRef = this.dialog.open(StopTrainingComponent, {
@@ -19,16 +25,30 @@ export class CurrentTrainingComponent implements OnInit {
       }
     });
 
+    // subscribes to the result of the dialog selection
     dialogRef.afterClosed().subscribe(result => {
       console.log(result);
+      if (result) {
+        this.trainingExit.emit();
+      } else {
+        this.startOrResumeTimer();
+      }
     });
   }
-  ngOnInit(): void {
+
+  /**
+   * starts or resumes the timer.
+   */
+  startOrResumeTimer(): void {
     this.timer = setInterval(() => {
-      this.progress += 20;
+      this.progress += 5;
       if (this.progress >= 100) {
         clearInterval(this.timer);
       }
     }, 1000);
+  }
+
+  ngOnInit(): void {
+    this.startOrResumeTimer();
   }
 }

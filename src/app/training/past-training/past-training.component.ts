4de46fdf9a6c0b1beb8da823373f077f0dd5b865
Ben Halverson <benhalverson33@gmail.com>
2020-06-04 00:49:00 -0700

    docs

--- src/app/training/past-training/past-training.component.ts
+++ src/app/training/past-training/past-training.component.ts
@@ -21,14 +21,20 @@ export class PastTrainingComponent implements OnInit, AfterViewInit, OnDestroy {
   displayedColumns = ['date', 'name', 'duration', 'calories', 'state'];
   dataSource = new MatTableDataSource<Exercise>();
   private exChangedSubscription: Subscription;
+
   constructor(private trainingService: TrainingService) {}
 
   @ViewChild(MatSort) sort: MatSort;
   @ViewChild(MatPaginator) paginator: MatPaginator;
 
+  /**
+   * Filters the data on the data table by removing extra spaces and lower casing the text.
+   * @param fitlerValue The text value the user entered in the search box.
+   */
   applyFilters(fitlerValue: string) {
     this.dataSource.filter = fitlerValue.trim().toLowerCase();
   }
+
   ngOnInit(): void {
     this.exChangedSubscription = this.trainingService.finishedExercisesChanged.subscribe(
       (exercises: Exercise[]) => {
